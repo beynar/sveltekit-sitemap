02685327698e5b763df12c5f8b6405444f23c344
Beynar <21969002+beynar@users.noreply.github.com>
2023-01-02 20:00:05 +0100

    add publish script

---
+++ sveltekit-sitemap/increment.js
@@ -0,0 +1,11 @@
+// @ts-nocheck
+const fs = require("fs");
+const semver = require("semver");
+(() => {
+  let package = JSON.parse(fs.readFileSync("./package.json"));
+  package.version = semver.inc(package.version, "patch");
+  fs.writeFileSync("./package.json", JSON.stringify(package, null, 3));
+
+  const publicationPackage = { ...package, types: "index.d.ts", main: "index.js" };
+  fs.writeFileSync("./dist/package.json", JSON.stringify(publicationPackage, null, 3));
+})();
